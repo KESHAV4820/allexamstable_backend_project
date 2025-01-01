318e57cb02e10cf20818d95e22f4388ebd3e3b63
KESHAV4820 <keshav48kumar@gmail.com>
2025-01-01 17:44:18 +0530

    debugging process

--- backendMiddlewares/dataStreamingforViewmiddleware.js
+++ backendMiddlewares/dataStreamingforViewmiddleware.js
@@ -3,6 +3,7 @@ const { getRecordsByFiltersDataStream } = require('../sqlscripts/queryToStreamDa
 const processCancellationManager = require('./../controller/processCancellationManager');
 const {pool, RequestTracker, QueryManager, comprehensiveRequestMiddleware} = require('../backendMiddlewares/processId_tracking_closing');
 
+//Note without destructuing the streamingObject named streamObj
 const streamRecordsMiddleware = async (req, res) => {
   console.log('Stream request received');// Code Testing
   
@@ -14,6 +15,7 @@ const streamRecordsMiddleware = async (req, res) => {
       console.log('Starting stream process');//Code Testing
       
       const client = await pool.connect();
+      let streamObj;// newly added1/1/2025
       
       try {
         QueryManager.trackQuery(clientId, client);
@@ -25,7 +27,8 @@ const streamRecordsMiddleware = async (req, res) => {
         // console.log(`streamRecordsMiddleware line:21  ${req.body}, ${client}`);// Code Testing
         
         // const dataStream = await getRecordsByFiltersDataStream(req.body, client);Bug found ConceptNote: dataStream is an object. It has stream, cleanup function in it. we can't use this object directly. We have to destructure it to call .pipe function.
-        const {stream, cleanup} = await getRecordsByFiltersDataStream(req.body, client);
+        // const {stream, cleanup} = await getRecordsByFiltersDataStream(req.body, client);
+        const streamObj = await getRecordsByFiltersDataStream(req.body.filters || req.body, client);//newly added1/1/2025
         console.log('Stream created');//Code Testing
         
         
@@ -43,7 +46,7 @@ const streamRecordsMiddleware = async (req, res) => {
         });
 
         // dataStream//wrong. we can't use this object itself
-        stream
+        streamObj.stream//newly added
           .pipe(transform)
           .pipe(res)
           .on('error', (error) => {
@@ -58,10 +61,10 @@ const streamRecordsMiddleware = async (req, res) => {
 
           //Adding debug log for empty results
           let hasData = false;
-          stream.on('data', () => {
+          streamObj.stream.on('data', () => {//newly added
             hasData = true;
           });// For Code Testing only
-          stream.on('end', () => {
+          streamObj.stream.on('end', () => {//newly added
             if (!hasData) {
               console.log('Streaming completed with no more data found');
             }
@@ -69,12 +72,18 @@ const streamRecordsMiddleware = async (req, res) => {
 
         req.on('close', () => {
           console.log('Request closed');//Code Testing
-          dataStream.destroy();
+          // dataStream.destroy();//Issue Foundfrom where did dataStream variable came!! it's no more in use at the first place. we destructured this object in the first place.
+          if(streamObj && streamObj.stream){//newly added
+          streamObj.stream.destroy();//Issue Resolved
+          };
           QueryManager.cancelQuery(clientId);
         });
 
       } finally {
         QueryManager.removeQuery(clientId);
+        if (streamObj && streamObj.cleanup) {
+          await streamObj.cleanup();
+        }
         client.release();
       }
     }
@@ -91,5 +100,94 @@ const streamRecordsMiddleware = async (req, res) => {
     }
   }
 };
+//
+/* Note with destructured streaming object
+const streamRecordsMiddleware = async (req, res) => {
+  console.log('Stream request received');
+  
+  const clientId = req.headers['x-client-id'];
+  const processCancellationToken = req.processCancellationToken;
 
+  const streamProcess = processCancellationManager.createCancellableProcess(
+    async (processToken, cancellationCheck) => {
+      console.log('Starting stream process');
+      
+      const client = await pool.connect();
+      
+      try {
+        QueryManager.trackQuery(clientId, client);
+
+        res.setHeader('Content-Type', 'text/event-stream');
+        res.setHeader('Cache-Control', 'no-cache');
+        res.setHeader('Connection', 'keep-alive');
+        
+        // Properly destructure the stream and cleanup
+        const { stream, cleanup } = await getRecordsByFiltersDataStream(req.body.filters || req.body, client);
+        console.log('Stream created');
+        
+        const transform = new Transform({
+          objectMode: true,
+          transform(chunk, _, callback) {
+            if (RequestTracker.shouldCancelRequest(req.path, clientId)) {
+              callback(new Error('Request cancelled'));
+              return;
+            }
+            callback(null, `data: ${JSON.stringify(chunk)}\n\n`);
+          }
+        });
+
+        stream
+          .pipe(transform)
+          .pipe(res)
+          .on('error', (error) => {
+            console.error('Stream error:', error);
+            QueryManager.cancelQuery(clientId);
+            res.status(500).end();
+          })
+          .on('end', () => {
+            console.log('Stream ended');
+            res.end();
+          });
+
+        let hasData = false;
+        stream.on('data', () => {
+          hasData = true;
+        });
+        
+        stream.on('end', () => {
+          if (!hasData) {
+            console.log('Streaming completed with no more data found');
+          }
+        });
+
+        req.on('close', () => {
+          console.log('Request closed');
+          if (stream) {
+            stream.destroy();
+          }
+          QueryManager.cancelQuery(clientId);
+        });
+
+      } finally {
+        QueryManager.removeQuery(clientId);
+        if (cleanup) {
+          await cleanup();
+        }
+        client.release();
+      }
+    }
+  );
+
+  try {
+    await streamProcess(processCancellationToken);
+  } catch (error) {
+    if (error.code === '57014') {
+      res.status(499).json({ error: 'Query cancelled' });
+    } else {
+      console.error('Error streaming records:', error);
+      res.status(500).json({ error: 'Failed to stream records' });
+    }
+  }
+};
+*/
 module.exports = {streamRecordsMiddleware};
